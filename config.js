a0e8477e1fa63481554b2783975f605f6c4b5010
Trinh Bach <soulbodyx@gmail.com>
2024-05-17 03:51:03 +0700

    add customPreparation

--- config.js
+++ config.js
@@ -1,5 +1,23 @@
 const config = {
-    TOTAL_PAGE : 30
-}
+  TOTAL_PAGE: 5,
+  delay : {
+    betweenPage : 500,
+    waitForNav : 1000,
+    beforeScrape: 5000,
+  },
+  querySelector: {
+    navButtonPath : 'a[title="Go to next page"]'
+  },
+  filter: {
+    date: [
+      {
+        type: "Ngày nộp đơn",
+        id: "AFDT-filter",
+        searchTerm: 'input[placeholder="ví dụ 31.12.2017"]',
+        value: "2006.06.27 TO 2024.05.17"
+      },
+    ],
+  },
+};
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
-const config = require('./config')
+const config = require("./config");
 let currentPage = 0;
-
+let page;
 let initPage =
   "http://wipopublish.ipvietnam.gov.vn/wopublish-search/public/trademarks?1&query=OFCO:VN#";
-let filterString = "Số đơn gốc";
+let filter_sodongoc = "Số đơn gốc";
+let filter_ngaynopdon = "Ngày nộp đơn";
 let response1filter =
   "IBehaviorListener.0-body-basicSearchTab-searchInputPanel-searchForm-searchSubmitLink&query=OFCO:VN";
 let response2filter =
@@ -15,7 +16,6 @@ let response3filter =
 let responseNavigatorFilter =
   ".IBehaviorListener.0-body-searchResultPanel-resultWrapper-dataTable-bottomToolbars-toolbars-2-span-navigator-next&query=OFCO:VN";
 let outputfilePath = "output.txt";
-let navButtonPath = 'a[title="Go to next page"]';
 
 puppeteer
   .launch({
@@ -26,94 +26,126 @@ puppeteer
     },
   })
   .then(async (browser) => {
-    const page = await browser.newPage();
+    page = await browser.newPage();
     await page.goto(initPage, {
       timeout: 120000,
     });
     await page.content();
 
-    let allGood = await babyStep(page);
+    //await regularPreparation();
+    await customPreparation();
+    SortAndOrder();
+    await delay(config.delay.beforeScrape);
+    console.log("All good");
+    while (currentPage < config.TOTAL_PAGE) {
+      await delay(config.delay.betweenPage);
+      await scrape(page);
 
-    // start scraping
-    if (allGood) {
-      await delay(5000);
-      console.log("ALl good");
-      while (currentPage < config.TOTAL_PAGE) {
-        await delay(1000);
-        await scrape(page);
-
-        await page.waitForSelector(navButtonPath);
-        await delay(2000);
-        await page.evaluate(() => {
-          document.querySelector('a[title="Go to next page"]').click();
-        });
-        try {
-          console.log("waiting for response ...");
-          let response = await page.waitForResponse(
-            (res) =>
-              res.request().url().includes(responseNavigatorFilter) &&
-              res.request().method() == "GET" &&
-              res.status() == 200,
-            {
-              timeout: 10000,
-            }
-          );
-          currentPage++;
-        } catch (error) {
-          console.log("break !!!");
-          break;
-        }
+      await page.waitForSelector(config.querySelector.navButtonPath);
+      await delay(config.delay.waitForNav);
+      await page.evaluate((path) => {
+        document.querySelector(path).click();
+      }, config.querySelector.navButtonPath);
+      try {
+        console.log("waiting for response ...");
+        let response = await page.waitForResponse(
+          (res) =>
+            res.request().url().includes(responseNavigatorFilter) &&
+            res.request().method() == "GET" &&
+            res.status() == 200,
+          {
+            timeout: 10000,
+          }
+        );
+        currentPage++;
+      } catch (error) {
+        console.log("break !!!");
+        break;
       }
     }
+
     console.log("Done");
   });
 
-async function babyStep(page) {
+async function regularPreparation() {
   console.log("clicking #sortField ...");
   await delay(1000);
   await page.waitForSelector("#sortField");
   let sortField = await page.$("#sortField");
-  await sortField.select(filterString);
-  let response1 = await page.waitForResponse(
-    (res) =>
-      res.request().url().includes(response1filter) &&
-      res.request().method() == "POST" &&
-      res.status() == 200
+  await sortField.select(filter_sodongoc);
+
+  await waitForResponse(response1filter, "POST");
+}
+
+async function customPreparation() {
+  let dateTypeFilter = config.filter.date.find(
+    (item) => item.type === "Ngày nộp đơn"
+  ); // config.date.type = 'Ngày nộp đơn' // click
+  await page.evaluate((id) => {
+    document.querySelector(`#${id}`).click();
+  }, dateTypeFilter.id);
+
+  await waitForResponse(
+    "IBehaviorListener.0-body-advancedSearchTab-preferencesPanel-categories-3-criterias-criteriasWrapper-1-filter&query=OFCO:VN",
+    "GET"
   );
 
-  let response2;
-  if (response1 !== undefined) {
-    console.log("Clicking #linesLink-iLink ...");
-    await delay(1000);
-    await page.waitForSelector("#linesLink-iLink");
-    await page.evaluate(() =>
-      document.querySelector("#linesLink-iLink").click()
-    );
-    response2 = await page.waitForResponse(
+  let input = await page.$(dateTypeFilter.searchTerm);
+  await input.type(dateTypeFilter.value, { delay: 200 });
+  await page.evaluate(() => {
+    document.querySelector('#advanceSearchButton').click()
+  });
+
+  await waitForResponse(
+    "IBehaviorListener.0-body-advancedSearchTab-advancedSearchInputPanel-advancedSearchForm-advancedSearchSubmitLink&query=OFCO:VN",
+    "POST"
+  );
+
+  await delay(1000);
+  await page.waitForSelector("#sortField");
+  let sortField = await page.$("#sortField");
+  await sortField.select(filter_ngaynopdon);
+  await waitForResponse(
+    "IBehaviorListener.0-body-basicSearchTab-searchInputPanel-searchForm-searchSubmitLink&query=OFCO:VN",
+    "POST"
+  );
+
+  console.log("Clicking .asc-icon ...");
+  await delay(1000);
+  await page.waitForSelector(".asc-icon");
+  await page.evaluate(() => {
+    document.querySelector(".asc-icon").click();
+  });
+  await waitForResponse(response3filter, "POST");
+}
+
+async function waitForResponse(url, method) {
+  try {
+    await page.waitForResponse(
       (res) =>
-        res.request().url().includes(response2filter) &&
-        res.request().method() == "GET" &&
+        res.request().url().includes(url) &&
+        res.request().method() == method &&
         res.status() == 200
     );
+  } catch (error) {
+    throw error;
   }
+}
 
-  let response3;
-  if (response2 !== undefined) {
-    console.log("Clicking .asc-icon ...");
-    await delay(1000);
-    await page.waitForSelector(".asc-icon");
-    await page.evaluate(() => {
-      document.querySelector(".asc-icon").click();
-    });
-    response3 = await page.waitForResponse(
-      (res) =>
-        res.request().url().includes(response3filter) &&
-        res.request().method() == "POST" &&
-        res.status() == 200
-    );
-  }
+async function SortAndOrder() {
+  console.log("Clicking #linesLink-iLink ...");
+  await delay(1000);
+  await page.waitForSelector("#linesLink-iLink");
+  await page.evaluate(() => document.querySelector("#linesLink-iLink").click());
+  await waitForResponse(response2filter, "GET");
 
-  return response3 !== undefined;
+  console.log("Clicking .asc-icon ...");
+  await delay(1000);
+  await page.waitForSelector(".asc-icon");
+  await page.evaluate(() => {
+    document.querySelector(".asc-icon").click();
+  });
+  await waitForResponse(response3filter, "POST");
 }
 
 async function scrape(page) {
@@ -133,16 +165,18 @@ async function scrape(page) {
       ".rs-STLB",
     ];
 
-    const extractedData = classesToExtract.map(selector =>
-      Array.from(document.querySelectorAll(selector)).map(element => element.innerText.trim())
+    const extractedData = classesToExtract.map((selector) =>
+      Array.from(document.querySelectorAll(selector)).map((element) =>
+        element.innerText.trim()
+      )
     );
 
     // Get the maximum number of rows
-    const maxRows = Math.max(...extractedData.map(arr => arr.length));
+    const maxRows = Math.max(...extractedData.map((arr) => arr.length));
 
     // Create CSV content
     const csvRows = Array.from({ length: maxRows }, (_, i) =>
-      extractedData.map(column => column[i] || "").join("\t")
+      extractedData.map((column) => column[i] || "").join("\t")
     );
 
     const csvContent = [...csvRows].join("\n") + "\n";
